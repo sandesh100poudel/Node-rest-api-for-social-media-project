a5196fd6d56eb799e6184384c132d6f77341fc06
Sandesh Poudel <sandesh100poudel@gmail.com>
2022-05-20 03:41:02 -0700

    creating follow and unfollow api

--- models/User.js
+++ models/User.js
@@ -34,7 +34,7 @@ const userSchema= new mongoose.Schema({
          default:[]
      } ,
 
-     followins:{
+     followings:{
         type:Array,
         default:[]
     } ,
--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,10 @@ if(req.body.password){
         return res.status(403).json("you can updaate on you r accoundt")
     }
 })
+
+
+
+
 //delete user
 
 router.delete("/:id", async(req,res)=>{
@@ -44,7 +48,58 @@ try {
     }
 })
 //get a user
+
+router.get("/:id", async(req,res)=>{
+    try{
+        const user=await User.findById(req.params.id);
+        const{password,updatedAt, ...other}=user._doc
+        res.status(200).json(other)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 //follow user
+router.put("/:id/follow",async(req,res)=>{
+    if(req.body.userId !== req.params.id){
+      try{
+const user= await User.findById(req.params.id);
+const currentUser=await User.findById(req.body.userId);
+if(!user.followers.includes(req.body.userId)){
+    await user.updateOne({$push:{followers:req.body.userId}})
+    await currentUser.updateOne({$push:{followings:req.params.id}})
+res.status(200).json("user has been followed");
+      }else{
+          res.status(403).json("you already follow this user")
+      }
+     } catch(err){
+res.status(500).json(err);
+      }
+    }  else{
+       res.status(403).json("you cant follow yourself")     
+    }
+})
+
 //unfollow user
+
+router.put("/:id/unfollow",async(req,res)=>{
+    if(req.body.userId !== req.params.id){
+      try{
+const user= await User.findById(req.params.id);
+const currentUser=await User.findById(req.body.userId);
+if(user.followers.includes(req.body.userId)){
+    await user.updateOne({$pull:{followers:req.body.userId}})
+    await currentUser.updateOne({$pull:{followings:req.params.id}})
+res.status(200).json("user has been unfollowed");
+      }else{
+          res.status(403).json("you already unfollow this user")
+      }
+     } catch(err){
+res.status(500).json(err);
+      }
+    }  else{
+       res.status(403).json("you cant unfollow yourself")     
+    }
+})
 
 module.exports=router
