de17300f74ae9bac965a634bdab51955be9fd61f
Sandesh Poudel <sandesh100poudel@gmail.com>
2022-05-20 01:20:11 -0700

    creating update and delete api

--- routes/users.js
+++ routes/users.js
@@ -5,29 +5,44 @@ const bcrypt=require("bcrypt");
 
 //update user
 
-// router.put("/:id", async(req,res)=>{
-//     if(req.body.userId === req.params.id || req.user.isAdmin){
-// if(req.body.password){
-//     try{
-//         const salt =await bcrypt.genSalt(100);
-//         req.body.password=await bcrypt.hash(req.body.password, salt);
-//     }catch(err){
-//         return res.status(500).json(err);
-//     }
-// }try {
-//     const user=await User.findByIdAndUpdate(req.params.id, {
-//         $set:req.body,
-//     })
-//     res.status(200).json("account has been updated");
-// }catch(err){
-//     return res.status(500).json(err);
+router.put("/:id", async(req,res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+if(req.body.password){
+    try{
+        const salt =await bcrypt.genSalt(100);
+        req.body.password=await bcrypt.hash(req.body.password, salt);
+    }catch(err){
+        return res.status(500).json(err);
+    }
+}try {
+    const user=await User.findByIdAndUpdate(req.params.id, {
+        $set:req.body,
+    })
+    res.status(200).json("account has been updated");
+}catch(err){
+    return res.status(500).json(err);
     
-// }
-//     }else{
-//         return res.status(403).json("you can updaate on you r accoundt")
-//     }
-// })
+}
+    }else{
+        return res.status(403).json("you can updaate on you r accoundt")
+    }
+})
 //delete user
+
+router.delete("/:id", async(req,res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+
+try {
+    await User.findByIdAndDelete( req.params.id)
+    res.status(200).json("account has been deleted");
+}catch(err){
+    return res.status(500).json(err);
+    
+}
+    }else{
+        return res.status(403).json("you can deleted on you r accoundt")
+    }
+})
 //get a user
 //follow user
 //unfollow user
